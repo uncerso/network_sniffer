31c0ab884a5a30c82e756561382bf1239b13d8af
uncerso <yayayayaya38@gmail.com>
2019-04-20 01:11:50 +0300

    Added more ptotocols types. Improved makefile.

--- Makefile
+++ Makefile
@@ -1,17 +1,22 @@
 name = snif
 obj-m += $(name).o
+.phony: all clean unload load build app1
 all:
-	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+#	if [[$(lsmod | grep nc_kernel_module)]] then
+#	make unload
+	make build
+	make load
+	make clean
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
-upload:
-	sudo insmod $(name).ko
-end:
+unload:
 	sudo rmmod $(name)
-	make clean
-start:
-	make all
-	make upload
-test:
-	make upload
-	make end
+unloadFORCE:
+	sudo rmmod --force $(name)
+load:
+	sudo insmod $(name).ko
+build:
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+
+app1_:
+	g++ -Wall -O3 --std=c++17 app1.cpp -o app1
--- snif.c
+++ snif.c
@@ -12,63 +12,86 @@ int input = 1;
 int output = 0;
 
 unsigned int hook_func(void *priv,
-                    struct sk_buff *skb,
-                    const struct nf_hook_state *state)
+					struct sk_buff *skb,
+					const struct nf_hook_state *state)
 {
-    const char * direction_type;
-    if (priv == NULL) 
-        direction_type = "NULL direction??";
-    else if (*(int*)priv == input) 
-        direction_type = "input ";
-    else if (*(int*)priv == output) 
-        direction_type = "output";
-    else direction_type = "unknown direction??";
+	const char * direction_type;
+	if (priv == NULL) 
+		direction_type = "NULL direction??";
+	else if (*(int*)priv == input) 
+		direction_type = "input ";
+	else if (*(int*)priv == output) 
+		direction_type = "output";
+	else direction_type = "unknown direction??";
 
-    const char * packet_type;
-    const char * header_type;
+	const char * packet_type;
+	const char * header_type;
 
-    unsigned char from[4];
-    unsigned char to[4];
-    *(uint*)from = 0;
-    *(uint*)to = 0;
-    
-    if (skb->protocol == htons(ETH_P_IP)){
-        packet_type = "ip";
-        struct iphdr *ip = (struct iphdr *)skb_network_header(skb);
-        switch (ip->protocol){
-            case IPPROTO_TCP: header_type = "TCP"; break;
-            case IPPROTO_ICMP: header_type = "ICMP"; break;
-            case IPPROTO_UDP: header_type = "UDP"; break;
-            default: header_type = "unknown";
-        }
-        *(uint*)from = ip->saddr;
-        *(uint*)to = ip->daddr;
-    }
-    else packet_type = "unknown";
-    
-    printk("Snif: %s %s %s packet, from %u.%u.%u.%u to %u.%u.%u.%u", direction_type, packet_type, header_type, from[0], from[1], from[2], from[3], to[0], to[1], to[2], to[3]);
-    return NF_ACCEPT;
+	unsigned char from[4];
+	unsigned char to[4];
+	*(uint*)from = 0;
+	*(uint*)to = 0;
+	int protocol_num = -1;
+	if (skb->protocol == htons(ETH_P_IP)){
+		packet_type = "ip";
+		struct iphdr *ip = (struct iphdr *)skb_network_header(skb);
+		switch (ip->protocol) {
+			case IPPROTO_ICMP	: header_type = "ICMP";		break;
+			case IPPROTO_IGMP	: header_type = "IGMP";		break;
+			case IPPROTO_IPIP	: header_type = "IPIP";		break;
+			case IPPROTO_TCP	: header_type = "TCP";		break;
+			case IPPROTO_EGP	: header_type = "EGP";		break;
+			case IPPROTO_PUP	: header_type = "PUP";		break;
+			case IPPROTO_UDP	: header_type = "UDP";		break;
+			case IPPROTO_IDP	: header_type = "IDP";		break;
+			case IPPROTO_TP		: header_type = "TP";		break;
+			case IPPROTO_DCCP	: header_type = "DCCP";		break;
+			case IPPROTO_IPV6	: header_type = "IPV6";		break;
+			case IPPROTO_RSVP	: header_type = "RSVP";		break;
+			case IPPROTO_GRE	: header_type = "GRE";		break;
+			case IPPROTO_ESP	: header_type = "ESP";		break;
+			case IPPROTO_AH		: header_type = "AH";		break;
+			case IPPROTO_MTP	: header_type = "MTP";		break;
+			case IPPROTO_BEETPH	: header_type = "BEETPH";	break;
+			case IPPROTO_ENCAP	: header_type = "ENCAP";	break;
+			case IPPROTO_PIM	: header_type = "PIM";		break;
+			case IPPROTO_COMP	: header_type = "COMP";		break;
+			case IPPROTO_SCTP	: header_type = "SCTP";		break;
+			case IPPROTO_UDPLITE: header_type = "UDPLITE";	break;
+			case IPPROTO_MPLS	: header_type = "MPLS";		break;
+			case IPPROTO_RAW	: header_type = "RAW";		break;
+			case 200			: header_type = "NC";		break;
+			default				: header_type = "unknown";
+		}
+		protocol_num = ip->protocol;
+		*(uint*)from = ip->saddr;
+		*(uint*)to = ip->daddr;
+	}
+	else packet_type = "unknown";
+	
+	printk("Snif: %s %s %s(%d) packet, from %u.%u.%u.%u to %u.%u.%u.%u\n", direction_type, packet_type, header_type, protocol_num, from[0], from[1], from[2], from[3], to[0], to[1], to[2], to[3]);
+	return NF_ACCEPT;
 }
 
 int init_func(void){
-    printk(KERN_INFO "Snif: Init!\n");
-    inp_ops.hook = hook_func;
-    inp_ops.priv = &input;
-    inp_ops.pf = PF_INET;
-    inp_ops.hooknum = NF_INET_PRE_ROUTING;
-    inp_ops.priority = NF_IP_PRI_LAST;
-    out_ops = inp_ops;
-    out_ops.priv = &output;
-    out_ops.hooknum = NF_INET_POST_ROUTING;
-    nf_register_net_hook(&init_net, &inp_ops); 
-    nf_register_net_hook(&init_net, &out_ops); 
-    return 0;
+	printk(KERN_INFO "Snif: Init!\n");
+	inp_ops.hook = hook_func;
+	inp_ops.priv = &input;
+	inp_ops.pf = PF_INET;
+	inp_ops.hooknum = NF_INET_PRE_ROUTING;
+	inp_ops.priority = NF_IP_PRI_LAST;
+	out_ops = inp_ops;
+	out_ops.priv = &output;
+	out_ops.hooknum = NF_INET_POST_ROUTING;
+	nf_register_net_hook(&init_net, &inp_ops); 
+	nf_register_net_hook(&init_net, &out_ops); 
+	return 0;
 }
 
 void exit_func(void){
-    nf_unregister_net_hook(&init_net, &out_ops);
-    nf_unregister_net_hook(&init_net, &inp_ops);
-    printk(KERN_INFO "Snif: Exit!\n");
+	nf_unregister_net_hook(&init_net, &out_ops);
+	nf_unregister_net_hook(&init_net, &inp_ops);
+	printk(KERN_INFO "Snif: Exit!\n");
 }
 
 module_init(init_func);
